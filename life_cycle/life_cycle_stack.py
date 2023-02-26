335f44b2be8838869987ee66d6efdc3560e2d216
ShrirajChohan32 <45475446+ShrirajChohan32@users.noreply.github.com>
2023-02-26 21:02:58 +1300

    Added an execution role

--- life_cycle/life_cycle_stack.py
+++ life_cycle/life_cycle_stack.py
@@ -32,9 +32,68 @@ class LifeCycleStack(Stack):
 
         cdk.Tags.of(volume).add("backup", "yes")
 
+        policy_doc = iam.PolicyDocument(
+            statements=[
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "ec2:CreateSnapshot",
+                        "ec2:CreateSnapshots",
+                        "ec2:DeleteSnapshot",
+                        "ec2:DescribeInstances",
+                        "ec2:DescribeVolumes",
+                        "ec2:DescribeSnapshots",
+                        "ec2:EnableFastSnapshotRestores",
+                        "ec2:DescribeFastSnapshotRestores",
+                        "ec2:DisableFastSnapshotRestores",
+                        "ec2:CopySnapshot",
+                        "ec2:ModifySnapshotAttribute",
+                        "ec2:DescribeSnapshotAttribute",
+                        "ec2:DescribeSnapshotTierStatus",
+                        "ec2:ModifySnapshotTier"
+                    ],
+                    resources=["*"]
+                ),
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "ec2:CreateTags"
+                    ],
+                    resources=["arn:aws:ec2:*::snapshot/*"]
+                ),
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "events:PutRule",
+                        "events:DeleteRule",
+                        "events:DescribeRule",
+                        "events:EnableRule",
+                        "events:DisableRule",
+                        "events:ListTargetsByRule",
+                        "events:PutTargets",
+                        "events:RemoveTargets"
+                    ],
+                    resources=["arn:aws:events:*:*:rule/AwsDataLifecycleRule.managed-cwe.*"]
+                )
+            ]
+        )
+
+        role = iam.Role(
+            self, "MyRole",
+            assumed_by=iam.ServicePrincipal("dlm.amazonaws.com"),
+            inline_policies={
+                "MyPolicy": policy_doc
+            }
+        )
+
+        cdk.CfnOutput(self,"MyRoleName",value=role.role_arn)
+
+
         cfn_lifecycle_policy = dlm.CfnLifecyclePolicy(self, "MyCfnLifecyclePolicy",
             description="description",
-            execution_role_arn='arn:aws:iam::%s:role/service-role/AWSDataLifecycleManagerDefaultRole' % self.account,
+            execution_role_arn=role.role_arn,
+            # execution_role_arn='arn:aws:iam::%s:role/MyRole' % self.account
+            # execution_role_arn='arn:aws:iam::%s:role/service-role/AWSDataLifecycleManagerDefaultRole' % self.account,
             policy_details=dlm.CfnLifecyclePolicy.PolicyDetailsProperty(
                 policy_type="EBS_SNAPSHOT_MANAGEMENT",
                 resource_types=["VOLUME"],
@@ -42,11 +101,11 @@ class LifeCycleStack(Stack):
                     name='DLM_POLICY',
                     copy_tags=False,
                     create_rule=dlm.CfnLifecyclePolicy.CreateRuleProperty(
-        #                 # cron_expression="cron(15 12 * * ? *)",
+        #                 # cron_expression="cron(15 12 * * ? *)", for more customised 
                         interval=1,
                         interval_unit="HOURS",
                         location="CLOUD",
-                        times=["17:47"]),
+                        times=["07:40"]),
                     retain_rule=dlm.CfnLifecyclePolicy.RetainRuleProperty(
                         count=2))],
                 target_tags=[cdk.CfnTag(
@@ -55,7 +114,7 @@ class LifeCycleStack(Stack):
             ),
             state='ENABLED',
             tags=[cdk.CfnTag(
-                key="testing",
+                key="backup",
                 value="yes"
             )]
         )
